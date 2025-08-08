cc8eac3135fa75c38e39302fd0319f5374c03cd7
smhty <hussein.ty@gmail.com>
2025-08-08 14:10:00 -0700

    holder

--- object/__init__.py
+++ object/__init__.py
@@ -1,3 +1,5 @@
 from .item import *
+from .plate import *
+from .holder import *
 
 __version__ = "1.0.0"
--- object/plate.py
+++ object/plate.py
@@ -1,10 +1,10 @@
 from dorna2 import Pose
 
-class Charuco_plate(Pose):
-    def __init__(self, name="charuco_plate", pose=None, parent=None):
+class Charuco_board(Pose):
+    def __init__(self, name="Charuco_board", pose=None, parent=None):
         anchors = {
             "o": [-5, -5, -3, 0, 0, 0],
-            "xy": [215, 215, -3, 0, 0],
+            "xy": [215, 215, -3, 0, 0, 0],
             "x": [215, 0, -3, 0, 0, 0],
             "y": [0, 215, -3, 0, 0, 0],
         }

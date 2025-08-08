23e24a8aabe55ac4e0ae9b7f6850b84e853e5e47
smhty <hussein.ty@gmail.com>
2025-08-08 12:12:49 -0700

    charuco

--- object/plate.py
+++ object/plate.py
@@ -3,9 +3,9 @@ from dorna2 import Pose
 class Charuco_plate(Pose):
     def __init__(self, name="charuco_plate", pose=None, parent=None):
         anchors = {
-            "o": [-5, -5, 0, 0, 0, 0],
-            "xy": [215, 215, 0, 0, 0],
-            "x": [215, 0, 0, 0, 0, 0],
-            "y": [0, 215, 0, 0, 0, 0],
+            "o": [-5, -5, -3, 0, 0, 0],
+            "xy": [215, 215, -3, 0, 0],
+            "x": [215, 0, -3, 0, 0, 0],
+            "y": [0, 215, -3, 0, 0, 0],
         }
         super().__init__(name, pose, parent, anchors)

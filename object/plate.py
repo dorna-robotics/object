d2e56ecaf3e2bc0ff1128af6259576fbf7889623
smhty <hussein.ty@gmail.com>
2025-08-10 18:07:10 -0700

    plate

--- object/plate.py
+++ object/plate.py
@@ -8,4 +8,21 @@ class Charuco_board(Pose):
             "x": [215, 0, 3, 0, 0, 0],
             "y": [0, 215, 3, 0, 0, 0],
         }
+
+        for i in range(3):
+            anchors[str(i)] = [(2*i+1)*30+3, 0, 0, 0, 0, 0]
+        for i in range(4):
+            anchors[str(3+i)] = [(2*i)*30+3, (1)*30+3, 0, 0, 0, 0]
+        for i in range(3):
+            anchors[str(7+i)] = [(2*i+1)*30+3, (2)*30+3, 0, 0, 0, 0]
+        for i in range(4):
+            anchors[str(10+i)] = [(2*i)*30+3, (3)*30+3, 0, 0, 0, 0]
+        for i in range(3):
+            anchors[str(14+i)] = [(2*i+1)*30+3, (4)*30+3, 0, 0, 0, 0]
+        for i in range(4):
+            anchors[str(17+i)] = [(2*i)*30+3, (5)*30+3, 0, 0, 0, 0]
+        for i in range(3):
+            anchors[str(18+i)] = [(2*i+1)*30+3, (6)*30+3, 0, 0, 0, 0]
+
+
         super().__init__(name, pose, parent, anchors)
